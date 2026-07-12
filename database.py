9a9686e299833c260867be8bd2001a90f50f8397
GutembergKSGomes <gutembergkaua276@gmail.com>
2026-07-12 00:21:40 -0300

    Adicionado 2 páginas html (index e atendimento, mas atendimento ainda não funcional)

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 #
 # Arquivo responsável por armazenar a lógica de conexão com o banco de dados
 #
-
+import os
 import psycopg2
+
+os.environ['PGCLIENTECODING'] = 'UTF-8'
+os.environ['LC_MESSAGES'] = 'English'
 
 conexao = psycopg2.connect(
     dbname="projeto_hospital",
