7f54855e0dc949e482c6f9ba606f906c66b79f1a
Victor Oliveira <victoroliver249@gmail.com>
2026-07-14 19:38:10 -0300

    [etapa 1] implementa consulta de tempo medio de atendimento por residente

--- routes/estatisticas.py
+++ routes/estatisticas.py
@@ -32,3 +32,32 @@ def estatisticas():
     ranking = get_ranking_residentes()
 
     return render_template('estatisticas.html', ranking_residentes=ranking)
+
+# Tempo médio de duração dos atendimentos por residente
+@estatisticas_bp.route("/tempo_medio_residentes", methods=["GET"])
+def tempo_medio_residentes():
+
+    cursor = database.conexao.cursor()
+
+    consulta = """
+        SELECT
+            p.nome,
+            ROUND(AVG(a.duracao_minutos), 2) AS tempo_medio
+        FROM atendimento a
+        INNER JOIN residente r
+            ON r.id_profissional = a.id_residente
+        INNER JOIN profissional prof
+            ON prof.id_pessoa = r.id_profissional
+        INNER JOIN pessoa p
+            ON p.id_pessoa = prof.id_pessoa
+        GROUP BY p.nome
+        ORDER BY p.nome;
+    """
+
+    cursor.execute(consulta)
+
+    residentes = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template("tempo_medio_residentes.html",residentes=residentes)
