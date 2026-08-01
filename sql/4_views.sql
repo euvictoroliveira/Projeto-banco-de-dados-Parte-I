1d7e9089bfc7bea90dc9df142c0e531f841458d9
Victor Oliveira <victoroliver249@gmail.com>
2026-08-01 16:35:09 -0300

    [etapa 2] - adiciona view de residentes sem supervisão

--- sql/4_views.sql
+++ sql/4_views.sql
@@ -12,3 +12,46 @@ from (
 
 inner join pessoa p on i.id_paciente = p.id_pessoa
 where i.data_hora_saida is null;
+
+-- View de residentes sem supervisor
+CREATE OR REPLACE VIEW vw_residentes_sem_supervisor AS
+SELECT DISTINCT
+    r.id_profissional AS id_residente,
+    p_res.nome AS nome_residente,
+
+    pr.id_profissional AS id_preceptor,
+    p_pre.nome AS nome_preceptor,
+    pr.titulacao,
+
+    u.nome AS unidade,
+
+    e.dia_semana,
+    e.turno,
+    e.dia_plantao,
+    e.mes_plantao,
+    e.ano_plantao
+
+FROM escala e
+
+INNER JOIN residente r
+    ON e.id_residente = r.id_profissional
+
+INNER JOIN profissional prof_res
+    ON r.id_profissional = prof_res.id_pessoa
+
+INNER JOIN pessoa p_res
+    ON prof_res.id_pessoa = p_res.id_pessoa
+
+INNER JOIN preceptor pr
+    ON e.id_preceptor = pr.id_profissional
+
+INNER JOIN profissional prof_pre
+    ON pr.id_profissional = prof_pre.id_pessoa
+
+INNER JOIN pessoa p_pre
+    ON prof_pre.id_pessoa = p_pre.id_pessoa
+
+INNER JOIN unidade u
+    ON e.id_unidade = u.id_unidade
+
+WHERE pr.titulacao <> 'Doutor';
